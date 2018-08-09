b2e72c61c711c067d16556348381ef680425a9e5
Queco SM <sersalazar.marioni@gmail.com>
2018-08-09 03:38:02 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,30 @@
+class Person
+   def initialize
+      puts "Create an animal"
+   end
+   
+   def set_name(new_name)
+      @name = new_name
+   end
+
+   def name
+      @name
+   end
+
+   def name=(new_name)
+      @name = new_name
+   end
+   
+   def set_breed(new_breed)
+      @breed = new_breed
+   end
+
+   def breed
+      @breed
+   end
+
+   def breed=(new_breed)
+      @breed = new_breed
+   end
+   
+end
